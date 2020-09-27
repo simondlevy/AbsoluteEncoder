f53c09c0a425ff9d3375dffc2ec2210078672dd4
Levy <levys@wlu.edu>
2020-09-27 14:24:49 -0400

    More LP35 work

--- LP35/arduino/AbsoluteEncoder/AbsoluteEncoder.ino
+++ LP35/arduino/AbsoluteEncoder/AbsoluteEncoder.ino
@@ -1,8 +1,10 @@
 // https://forum.arduino.cc/index.php?topic=110250.0
 
-const int PIN_CLK_NEG = 5;
-const int PIN_CLK_POS = 6;
-const int PIN_DATA    = 7;
+static const uint8_t PIN_CLK_NEG = 5;
+static const uint8_t PIN_CLK_POS = 6;
+static const uint8_t PIN_DATA    = 7;
+
+static const uint8_t BIT_RESOLUTION  = 10;
 
 static void clockHigh(void)
 {
@@ -24,21 +26,33 @@ void setup(void)
     pinMode(PIN_CLK_POS, OUTPUT);
     pinMode(PIN_DATA, INPUT);
 
+    clockLow();
 }
 
 void loop(void) 
 {
     uint16_t pos = 0;
 
-    for (uint8_t i=0; i<10; i++) {
+    for (uint8_t i=0; i<BIT_RESOLUTION; i++) {
 
+        // Rising edge triggers transmission of next bit from encoder
+        clockHigh();
+
+        // Read next bit
+        uint16_t b = digitalRead(PIN_DATA) == HIGH ? 1 : 0;
+
+        // We start with MSB, so shift left
+        pos |= b<<(BIT_RESOLUTION-(i+1));
+
+        // Send falling edge
         clockLow();
 
-        uint8_t b = digitalRead(PIN_DATA) == HIGH ? 1 : 0;
-        pos += b * pow(2, 10-(i+1));
-
-        clockHigh();
+        // We need to wait between 2 and 10 usec between sending falling edges
+        delayMicroseconds(4);
     }
+
+    // We need to wait between 10 and 30 usec for next value
+    delayMicroseconds(20);
 
     Serial.println(pos);
 }
